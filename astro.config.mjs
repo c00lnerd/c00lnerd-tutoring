5d7932179d5276dc6a21250ef928699cddbaf177
c00lnerd <c00lnerd@gmail.com>
2025-07-19 15:47:28 -0500

    fix: add explicit Astro config for static assets and build output

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    assets: '_assets',
+  },
+  outDir: './dist',
+  publicDir: './public',
+  base: '/',
+});
